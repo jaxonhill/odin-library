e3549efe6bef3e8b7f6f41301eebfffad7bba0fb
jaxonhill <jaxoncharleshill@gmail.com>
2022-06-30 21:59:49 -0700

    Add query selectors for grid and btn

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function addBookToLibrary(bookObj) {
     myLibrary.push(bookObj);
 }
 
+// Make sure we have the card container grid and add button in the JS
+cardContainer = document.querySelector('.card-container');
+addButton = document.querySelector('.add-btn');
+
+// Add a click event listener so we can pop up the form
+addButton.addEventListener('click', {});
+
+// TODO: NEED TO ADD A MODAL FORM WHEN YOU CLICK THE ADD BTN
