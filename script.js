903a5ddf16f73ad26e4e189f979dc4d585ca9504
jaxonhill <jaxoncharleshill@gmail.com>
2022-07-02 16:46:13 -0700

    Finish up form functionality, can now add book

--- script.js
+++ script.js
@@ -16,10 +16,117 @@ function addBookToLibrary(bookObj) {
 }
 
 // Make sure we have the card container grid and add button in the JS
-cardContainer = document.querySelector('.card-container');
-addButton = document.querySelector('.add-btn');
+const cardContainer = document.querySelector('.card-container');
+const addButton = document.querySelector('[data-modal-target]');
+const closeButton = document.querySelector('.x-btn');
+const overlay = document.getElementById('overlay');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const yearInput = document.querySelector('#year');
+const genreInput = document.querySelector('#genre');
+const addBookButton = document.querySelector('#submit');
 
-// Add a click event listener so we can pop up the form
-addButton.addEventListener('click', {});
+// Add a click event listener so we can pop up the form with add button
+addButton.addEventListener('click', () => {
+    const modal = document.querySelector(addButton.dataset.modalTarget);
+    openModal(modal);
+});
 
-// TODO: NEED TO ADD A MODAL FORM WHEN YOU CLICK THE ADD BTN
+// Add a click event so we can close the form with the close button
+closeButton.addEventListener('click', () => {
+    const modal = closeButton.closest('.modal');
+    closeModal(modal);
+});
+
+addBookButton.addEventListener('click', () => {
+    console.log(titleInput.value);
+    if (titleInput.value && authorInput.value) {
+        let novel = new Book(titleInput.value, authorInput.value, pagesInput.value, yearInput.value, genreInput.value);
+        // addBookToLibrary(newBook);
+        // For each book object in the array, create a new card
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        // Create and append the children elements needed for a card
+        const h2 = document.createElement('h2');
+        h2.textContent = novel.title;
+        card.appendChild(h2);
+
+        const pAuthor = document.createElement('p');
+        pAuthor.innerHTML = `<strong>Author: </strong>${novel.author}`;
+        card.appendChild(pAuthor);
+
+        const pPages = document.createElement('p');
+        pPages.innerHTML = `<strong>Pages: </strong>${novel.pages}`;
+        card.appendChild(pPages);
+
+        const pYear = document.createElement('p');
+        pYear.innerHTML = `<strong>Year Published: </strong>${novel.year}`;
+        card.appendChild(pYear);
+
+        const pGenre = document.createElement('p');
+        pGenre.innerHTML = `<strong>Genre: </strong>${novel.genre}`;
+        card.appendChild(pGenre);
+
+        // Finally append the card to the card container
+        cardContainer.appendChild(card);
+
+        // Clear the input fields
+        titleInput.value = '';
+        authorInput.value = '';
+        pagesInput.value = '';
+        yearInput.value = '';
+        genreInput.value = '';
+
+        // Then close the modal
+        const modal = closeButton.closest('.modal');
+        closeModal(modal);
+    } else {
+        alert("You must enter a TITLE and an AUTHOR");
+    }
+});
+
+myLibrary.forEach((novel) => {
+    // For each book object in the array, create a new card
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Create and append the children elements needed for a card
+    const h2 = document.createElement('h2');
+    h2.textContent = novel.title;
+    card.appendChild(h2);
+
+    const pAuthor = document.createElement('p');
+    pAuthor.innerHTML = `<strong>Author: </strong>${novel.author}`;
+    card.appendChild(pAuthor);
+
+    const pPages = document.createElement('p');
+    pPages.innerHTML = `<strong>Pages: </strong>${novel.pages}`;
+    card.appendChild(pPages);
+
+    const pYear = document.createElement('p');
+    pYear.innerHTML = `<strong>Year Published: </strong>${novel.year}`;
+    card.appendChild(pYear);
+
+    const pGenre = document.createElement('p');
+    pGenre.innerHTML = `<strong>Genre: </strong>${novel.genre}`;
+    card.appendChild(pGenre);
+
+    // Finally append the card to the card container
+    cardContainer.appendChild(card);
+});
+
+// Function for opening the modal, it will ADD active to both modal and overlay
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+// Function for opening the modal, it will REMOVE active to both modal and overlay
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
